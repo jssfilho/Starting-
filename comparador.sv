970bcd5d82bdb0b18084d115fdbae1393434bcf8
João <j_ssfilho@hotmail.com>
2018-12-17 19:57:58 -0200

    ...

--- comparador.sv
+++
@@ -1,4 +0,0 @@
-module comparador(a,b,s);
-	input [31:0] a, b;
-	output s;
-endmodule
--- somador_completo.sv
+++
@@ -1,7 +0,0 @@
-module somador_completo(a, b, cin, s, cout);
-    input a, b, cin;
-    output s, cout;
-    wire sm, cm, ct;
-    meio_somador MS1(a, b, sm, cm), MS2(sm, cin, s, ct);
-    assign cout = ct | cm;
-endmodule
